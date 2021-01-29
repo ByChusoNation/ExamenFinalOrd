f313479fac48dceaf50d5c9ffda5956e7dfd3fd7
Jesus <chusotejada@gmail.com>
2021-01-29 18:46:52 +0100

    Primer Commit creamos el vaadin y dependencias

---
+++ JT/src/test/java/ufv/dis/final2021/JT/JtApplicationTests.java
@@ -0,0 +1,13 @@
+package ufv.dis.final2021.JT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
