c6b5e5d340b21461aaa4d5c8eccc4d99126c9c8e
Jesus <chusotejada@gmail.com>
2021-01-29 20:02:43 +0100

    Añadimos anexo1

--- JT/src/main/java/ufv/dis/final2021/JT/IP_Direcciones.java
+++ JT/src/main/java/ufv/dis/final2021/JT/IP_Direcciones.java
@@ -9,73 +9,29 @@ public class IP_Direcciones {
         public static void main(String[] args) {
 
             JavaBitwiseExample obj = new JavaBitwiseExample();
-
-            System.out.println("iptoLong  : " + obj.ipToLong("192.168.1.2"));
-            System.out.println("iptoLong2 : " + obj.ipToLong2("192.168.1.2"));
-
-            System.out.println("longToIp  : " + obj.longToIp(3232235778L));
-            System.out.println("longToIp2 : " + obj.longToIp2(3232235778L));
         }
 
-        // example : 192.168.1.2
-        public long ipToLong(String ipAddress) {
-
-            // ipAddressInArray[0] = 192
-            String[] ipAddressInArray = ipAddress.split("\\.");
-
-            long result = 0;
-            for (int i = 0; i < ipAddressInArray.length; i++) {
-
-                int power = 3 - i;
-                int ip = Integer.parseInt(ipAddressInArray[i]);
-                result += ip * Math.pow(256, power);
-
+        public static Long Dot2LongIP(String dottedIP) {
+            String[] addrArray = dottedIP.split("\\.");
+            long num = 0;
+            for (int i=0;i<addrArray.length;i++) {
+                int power = 3-i;
+                num += ((Integer.parseInt(addrArray[i]) % 256) *
+                        Math.pow(256, power));
             }
-
-            return result;
-
+            return num;
         }
 
-        public long ipToLong2(String ipAddress) {
-
-            long result = 0;
-
-            String[] ipAddressInArray = ipAddress.split("\\.");
-
-            for (int i = 3; i >= 0; i--) {
-
-                long ip = Long.parseLong(ipAddressInArray[3 - i]);
-                result |= ip << (i * 8);
-
-            }
-
-            return result;
-        }
-
-        public String longToIp(long i) {
-
-            return ((i >> 24) & 0xFF) +
-                    "." + ((i >> 16) & 0xFF) +
-                    "." + ((i >> 8) & 0xFF) +
-                    "." + (i & 0xFF);
-
-        }
-
-        public String longToIp2(long ip) {
-            StringBuilder sb = new StringBuilder(15);
-
+        public String longToIp(long ip) {
+            StringBuilder result = new StringBuilder(15);
             for (int i = 0; i < 4; i++) {
-
-                sb.insert(0, Long.toString(ip & 0xff));
-
+                result.insert(0,Long.toString(ip & 0xff));
                 if (i < 3) {
-                    sb.insert(0, '.');
+                    result.insert(0,'.');
                 }
-
                 ip = ip >> 8;
-
             }
-
-            return sb.toString();
+            return result.toString();
         }
     }
+
